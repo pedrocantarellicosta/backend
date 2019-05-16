dc6c6b1d0dd47b6ab480b4e09df4460dc1949079
Pedro Cantarelli Costa <pedrocantarellicosta@live.com>
2019-05-16 11:19:20 -0300

    Manutencao no server

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-io.on('conection', socket => {
+io.on('connection', socket => {
     socket.on('connectRoom', box=>{
         socket.join(box);
     });
